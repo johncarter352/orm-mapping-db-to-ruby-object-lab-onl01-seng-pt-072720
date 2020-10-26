5ee2fd9fa1bb21177e1f04af6f3bf97adc864dbe
John Carter <johncarter352@gmail.com>
2020-10-26 06:31:21 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -112,7 +112,21 @@ class Student
       self.new_from_db(row)
     end.first
   end 
-  
+   
+  def self.
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 10 
+      LIMIT 1 
+    SQL
+    
+    
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end.first
+  end 
+   
 
 
 end
