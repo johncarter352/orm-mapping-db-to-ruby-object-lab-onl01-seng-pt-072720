fdeb617f09ddc8db29d99be1dbce701c0e011748
John Carter <johncarter352@gmail.com>
2020-10-26 06:24:19 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -66,7 +66,7 @@ class Student
       WHERE grade = 9
     SQL
     
-    DB[:conn].execute(sql).collect do |row|
+    DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
     end
   end
