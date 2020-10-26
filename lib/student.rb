6eb7b9e28f9d878ea52cf843ec2d362d429f3d9c
John Carter <johncarter352@gmail.com>
2020-10-26 06:12:59 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -60,17 +60,24 @@ class Student
   end
 
   def self.all_students_in_grade_9
-  sql = <<-SQL
-    SELECT *
-    FROM students
-    WHERE grade = 9
+    sql = <<-SQL
+     SELECT *
+      FROM students
+      WHERE grade = 9
     SQL
     
     DB[:conn].execute(sql)
   end
 
   def self.students_below_12th_grade
-
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade < 12
+    SQL
+    
+    DB[:conn].execute(sql)
+  end
 
 
 end
