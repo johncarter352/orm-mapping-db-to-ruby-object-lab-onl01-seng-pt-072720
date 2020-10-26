9c43cee9793cb62c992605c8d6694c2844ed4ccc
John Carter <johncarter352@gmail.com>
2020-10-26 06:09:34 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -59,7 +59,16 @@ class Student
     DB[:conn].execute(sql)
   end
 
-
+  
+  all_in_9 = self.all_students_in_grade_9
+  sql = <<-SQL
+    SELECT *
+    FROM students
+    WHERE grade = 9
+    SQL
+    
+    DB[:conn].execute(sql)
+    all_in_9 
 
 
 
